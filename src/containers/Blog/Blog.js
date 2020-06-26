95acd3fa68bb89f988c7d403697f166ef770b8fb
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-06-26 10:30:12 +0500

    My Commits

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import axios from '../../axios';
+
+import Post from '../../components/Post/Post';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount () {
+        axios.get( '/posts' )
+            .then( response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } )
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
+
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <section className="Posts">
+                    {posts}
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
