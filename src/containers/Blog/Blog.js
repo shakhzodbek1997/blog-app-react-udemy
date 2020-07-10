a47c76b52a6d874e60eb77d0537b88c4d3de9e65
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-07-10 12:38:36 +0500

    213. working with Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,11 @@ import Posts from './Posts/Posts';
 import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
+
     render() {
         return (
             <div className="Blog">
@@ -33,7 +38,7 @@ class Blog extends Component {
                 <Route path="/"   render={() => <h1>Home2</h1>}/>*/}
 
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" exact component={NewPost}/> : null}
                     <Route path="/posts"  component={Posts}/>
                     <Redirect from="/" to="/posts "/>
                     {/*<Route path="/"  component={Posts}/>*/}
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -13,6 +13,7 @@ class NewPost extends Component {
     }
 
     componentDidMount() {
+        // If unauth => this.props.history.replace('/posts');
         console.log(this.props);
     }
 
@@ -25,7 +26,8 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
-                this.setState({submitted: true});
+                this.props.history.replace('/posts');
+                // this.setState({submitted: true});
             });
     }
 
